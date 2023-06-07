7a00266e1c43a729d0a430068cec8735fc39b8d1
Corban-Lee <77944149+Corban-Lee@users.noreply.github.com>
2023-06-07 22:26:10 +0100

    removed old frontend files - replaced with new

--- WebShot/static/js/index.js
+++ WebShot/static/js/index.js
@@ -1,208 +1,135 @@
-$(document).ready(() => {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    const previousTheme = localStorage.getItem("theme");
-    if (previousTheme) {
-        loadTheme(previousTheme)
-    }
+/**
+ * Generates a random unique ID
+ */
+function generateUniqueId() {
+    const timestamp = new Date().getTime();
+    const random = Math.floor(Math.random() * 10000);
+    return `${timestamp}-${random}`;
+}
 
-    const previousBrowser = localStorage.getItem("browser");
-    if (previousBrowser) {
-        setBrowser(previousBrowser);
-    }
-});
-
-$("#searchForm").submit(function(e) {
-    e.preventDefault(); 
-
-    const url = $(this).find("input[type=search]").val();
-
-    if (!validUrl(url)) {
-        alert(`Invalid URL "${url}"`);
-        return;
-    }
-
-    const browser = localStorage.getItem("browser");
-
-    // create new task bar
-    $("#tasksContainer").append(`
-        <div class="px-3 py-2 bg-dark-subtle d-flex border-bottom" id="${browser}">
-            <span class="pagesProcessed me-3">
-                <span class="pagesProcessed-value">0</span>
-            </span>
-            <span>
-                <i class="bi bi-browser-${browser}"></i>
-            </span>
-            <nav class="breadcrumbs mx-3">
-                <ol class="breadcrumb mb-0">
-                </ol>
-            </nav>
-            <span class="timer ms-auto">
-                <span class="timer-value">0</span>
-            </span>
-        </div>
-    `);
-
-    var pagesProcessed = 0;
-
-    var secondsElapsed = 0.0;
-    var timerInterval = setInterval(() => {
-        secondsElapsed += 0.1;
-        $(`#${browser} .timer > .timer-value`).text(secondsElapsed.toFixed(1));
-    }, 100);
-
-    var eventSource = new EventSource("/stream-screenshots?url=" + encodeURIComponent(url) + "&browser=" + browser);
-    eventSource.onmessage = function(event) {
-        
-        const data = JSON.parse(event.data);
-        const screenshotPath = data.screenshotPath.replace(/\//g, '\\');
-        const browserType = data.browser;
-
-        pagesProcessed ++;
-        $(`#${browserType} .pagesProcessed > .pagesProcessed-value`).text(pagesProcessed);
-
-        createFileTreeItem(screenshotPath, browserType);
-
-        const fileName = screenshotPath.split("\\").slice(-1)[0];
-        var [width, height] = fileName.replace(".png", "").split(/x(.*)/s);
-
-        $("#outputRow").append(`
-            <div class="col-xxl-2 col-xl-3 col-lg-4 col-sm-6 mb-4">
-                <div class="position-relative">
-                <a type="button" href="${screenshotPath}" target="_blank">
-                    <div class="position-absolute top-0 start-0 m-1 bg-opacity-50 badge bg-secondary">
-                        ${width} x ${height}
-                    </div>
-                    <img src="${screenshotPath}" class="w-100 rounded shadow-sm"/>
-                    </a>
-                </div>
-            </div>
-        `);
-    }
-                // <div class="position-absolute top-0 end-0 m-1 bg-opacity-50 badge bg-secondary">
-                //     ${browserType}
-                // </div>
-    eventSource.onerror = function() {
-        console.error("An error occured while trying to connect [eventsource]");
-        eventSource.close();
-        $(`#${browser}`).removeClass("bg-dark-subtle").addClass("bg-primary-subtle")
-        clearInterval(timerInterval);
-    }
-});
-
+/**
+ * Tests the validity of a given URL.
+ * @param {string} url - The URL to validate.
+ * @returns {boolean} - True if the URL is valid
+ */
 function validUrl(url) {
     const urlPattern = /^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?$/i;
     return urlPattern.test(url);
 }
 
-$("#themeBtn").on("click", function() {
-    const currentTheme = $(document.body).attr("data-bs-theme");
-    const oppositeTheme = currentTheme == "dark" ? "light" : "dark";
+$(document).ready(function() {
 
-    loadTheme(oppositeTheme);
+    // Load the stored or default driver
+    var storedDriver = localStorage.getItem('driver');
+    storedDriver = !storedDriver ? "chrome" : storedDriver;
+    $(`input[name='drivers'][value='${storedDriver}']`).prop('checked', true);
+
+    // Store the selected driver
+    $("input[name='drivers']").on("click", function() {
+        localStorage.setItem("driver", $(this).val());
+    });
 });
 
-function loadTheme(theme) {
-    localStorage.setItem("theme", theme);
 
-    const icon = $("#themeBtn").find("i.bi");
-    $(document.body).attr("data-bs-theme", theme);
+$("#searchForm").submit(function(event) {
+    event.preventDefault();
 
-    if (theme == "light") {
-        icon.removeClass("bi-sun").addClass("bi-moon-stars");
-        $("#brand img").attr("src", logo);
+    const address = $(this).find("#search").val();
+
+    if (!validUrl(address)) {
+        alert("Please enter a valid web address");
+        return;
     }
-    else {
-        icon.removeClass("bi-moon-stars").addClass("bi-sun");
-        $("#brand img").attr("src", logoAlt);
+
+    const browser = $("input[name='drivers']:checked").val();
+
+    if (!browser) {
+        alert("Please select a driver from the drivers dropdown");
+        return;
     }
-}
+    
+    const encodedAddress = encodeURIComponent(address);
+    const eventSourceUrl = `/stream-screenshots?url=${encodedAddress}&browser=${browser}`;
 
-// $("#browserBtn").on("click", function() {
-//     const currentBrowser = localStorage.getItem("browser");
-//     const oppositeBrowser = currentBrowser == "chrome" ? "firefox" : "chrome";
-//     localStorage.setItem("browser", oppositeBrowser);
+    var eventSource = new EventSource(eventSourceUrl);
 
-//     setBrowser(oppositeBrowser);
-// });
-$("#browsersButtons button").each(function() {
-    $(this).on("click", function() {
-        setBrowser($(this).data("browser"));
+    $(window).off("beforeunload").on("beforeunload", function() {
+        return "Are you sure you want to leave this page? Your progress will be lost.";
     });
-});
 
-function setBrowser(browser) {
-    const icon = $("#browserBtn").find("i.bi");
-    icon.removeClass().addClass(`bi bi-browser-${browser}`);
-    localStorage.setItem("browser", browser);
-}
+    // Disable UI
+    $("#search").prop("disabled", true);
+    $("#searchBtn").prop("disabled", true);
+    $("input[name='drivers']").prop("disabled", true);
+    $("#btnDrivers").prop("disabled", true);
+    $("#collapseDrivers").removeClass("show");
 
-$("#expandAll").on("click", function() {
-    $(".filetree-container.collapse").each(function() {
-        bootstrap.Collapse.getOrCreateInstance("#" + this.id).show();
-    });
-});
+    // Show new task
+    $("#taskToast .timer").text("0.0");
+    $("#taskToast .breadcrumb").html("");
+    $("#taskToast .task-domain").text(address.split("://")[1]);
+    $("#taskToast .browser-icon").removeClass().addClass(`browser-icon bi bi-browser-${browser}`);
+    $("#taskToast").addClass("show");
 
-$("#collapseAll").on("click", function() {
-    $(".filetree-container.collapse").each(function() {
-        bootstrap.Collapse.getOrCreateInstance("#" + this.id).hide();
-    });
-});
+    var timer = 0.0
+    var timerInterval = setInterval(() => {
+        timer += 0.1;
+        $("#taskToast .timer").text(timer.toFixed(1));
+    }, 100);
 
-function showFileTreeItem(item, parent) {
-    if (item.type === "file") {
-            parent.prepend($(`
-                <li>
-                    <a class="text-body text-decoration-none" href="output/${item.path}" target="_blank">
-                        <div class="filetree-item">
-                            <i class="bi bi-browser-${item.browserType} mx-2 text-body-emphasis"></i>
-                            <span class="filetree-item-name">${item.name}</span>
-                        </div>
+    eventSource.onmessage = (event) => {
+        const data = JSON.parse(event.data)
+        const screenshotPath = data.screenshotPath;
+        const imageData = data.imageData;
+
+        addFiletreeItem(screenshotPath.replace(/\//g, '\\'), browser);
+
+        if ($(`#resultArea a[href="${screenshotPath}"]`).length === 0) {
+            // col-xxl-2 col-xl-3 col-lg-4 col-sm-6
+            $("#resultArea").append(`
+                <div class="col-xxl-2 col-xl-3 col-lg-4 col-sm-6" data-item="${screenshotPath}">
+                    <a href="data:image/png;base64,${imageData}" target="_blank">
+                        <img src="data:image/png;base64,${imageData}" class="w-100 rounded border shadow-sm">
                     </a>
-                </li>
-            `).css("--indent", item.indent));
-        }
-    else if (item.type === "folder") {
-        if (item.show) {
-            var chevron = "down"
-        }
-        else {
-            var chevron = "right"
-        }
-        parent.append($(`
-            <li data-path="${item.path}">
-                <div class="filetree-item" data-bs-toggle="collapse" data-bs-target="#collapseFolder-${item.path}" role="button">
-                    <i class="bi bi-chevron-${chevron} filetree-item-name"></i>
-                    <i class="bi bi-slash-lg mx-2 text-body-emphasis"></i>
-                    <span class="filetree-item-name">${item.name}</span>
                 </div>
-                <ul class="collapse filetree-container ${item.show}" id="collapseFolder-${item.path}"></ul>
-            </li>
-        `).css("--indent", item.indent));
+            `);
+        }
     }
-}
 
-function createFileTreeItem(data, browserType) {
-    const parts = data.split("\\").filter((part) => part !== "").slice(1);
+    eventSource.onerror = () => {
+        console.error("An error occured while trying to connect [eventsource]");
+        eventSource.close();
 
-    $(`#${browserType} .breadcrumbs ol`).html("");
+        // Enable UI
+        $("#search").prop("disabled", false);
+        $("#searchBtn").prop("disabled", false);
+        $("input[name='drivers']").prop("disabled", false);
+        $("#btnDrivers").prop("disabled", false);
 
-    for (var i = 0; i < parts.length; i++) {
-        const part = parts[i]
+        // Hide task
+        $("#taskToast").removeClass("show");
+        clearInterval(timerInterval);
+    }
+});
 
-        $(`#${browserType} .breadcrumbs ol`).append(`<li class="breadcrumb-item">${part}</li>`);
+function addFiletreeItem(screenshotPath, browser) {
+    const pathParts = screenshotPath.split("\\").filter((part) => part !== "").splice(1);
 
-        const type = i === parts.length - 1 ? "file" : "folder";
-        var path = parts.slice(0, i + 1).join("/");
-        const safePath = path.replace(/[.\/]/g, '\\$&')
-        const parentPath = parts.slice(0, i).join("/");
-        const safeParentPath = parentPath.replace(/[.\/]/g, '\\$&');
-        const exists = $(`#collapseFolder-${safeParentPath}`).length > 0;
-        
+    $("#taskToast .breadcrumb").html("");
+
+    for (var i = 0; i < pathParts.length; i++) {
+        const path = pathParts.slice(0, i + 1).join("/");
+
         if ($(`li[data-path="${path}"]`).length !== 0) {
             continue;
         }
+
+        const part = pathParts[i];
+        const type = i === pathParts.length - 1 ? "file" : "folder";
+        const safePath = path.replace(/[.\/]/g, '\\$&')
+        const parentPath = pathParts.slice(0, i).join("/");
+        const safeParentPath = parentPath.replace(/[.\/]/g, '\\$&');
 
         var item = {
             name: part,
@@ -210,17 +137,71 @@ function createFileTreeItem(data, browserType) {
             path: path,
             safePath: safePath,
             children: [],
-            browserType: browserType,
             show: i === 0 ? "show" : "",
-            indent: i + 1 + "rem"
         }
 
-        if (exists) {
-            var parent = $(`#collapseFolder-${safeParentPath}`);
-        }
-        else {
-            var parent = $("nav.filetree ul.filetree-container").first();
-        }
-        showFileTreeItem(item, parent)
+        $("#taskToast .breadcrumb").append(`<li class="breadcrumb-item">${part}</li>`);
+
+        var parent = $(`li[data-path="${safeParentPath}"]`).length > 0 ? $(`#collapse-${safeParentPath}`) : $("#filetree > ul").first();
+        showFiletreeItem(item, parent, browser)
     }
+}
+
+function showFiletreeItem(item, parent, browser) {
+    if (item.type === "file") {
+        const itemElement = $(`
+            <li data-path="${item.path}" class="filetree-item file">
+                <a href="output/${item.path}" target="_blank">
+                    <i class="bi bi-browser-${browser} me-1"></i>
+                    <span>${item.name}</span>
+                </a>
+            </li>
+        `);
+        parent.prepend(itemElement); // prepend so files appear before folders
+    }
+    else if (item.type === "folder") {
+        const itemElement = $(`
+            <li data-path="${item.path}" class="filetree-item folder">
+                <div data-bs-toggle="collapse" data-bs-target="#collapse-${item.path}" role="button">
+                    <i class="bi bi-chevron-right me-1"></i>
+                    <span>${item.name}</span>
+                </div>
+                <ul class="collapse" id="collapse-${item.path}"></ul>
+            </li>
+        `)
+        parent.append(itemElement);
+
+        // Change the chevron direction on showing collapse
+        itemElement.on("click", function(event) {
+            const icon = $(event.currentTarget).find(".bi").first();
+            const showing = $(event.currentTarget).find("div[data-bs-toggle='collapse']").first().hasClass("collapsed");
+
+            const transform = !showing ? "rotate(90deg)" : "";
+            icon.css("transform", transform);
+
+            event.stopPropagation();
+        });
+    }
+}
+
+$(document).ready(function() {
+    var useDarkMode = localStorage.getItem("option-useDarkMode");
+    setDarkMode(useDarkMode === "true");
+});
+
+$("#option-useDarkMode").on("click", function() {
+    const enable = $(this).prop("checked");
+    localStorage.setItem("option-useDarkMode", enable);
+    setDarkMode(enable);
+});
+
+function setDarkMode(enable) {
+    $("#option-useDarkMode").prop("checked", enable);
+
+    const theme = enable ? "dark" : "light"
+    $(document.body).attr("data-bs-theme", theme);
+
+    // Update the logo to match the theme
+    const sidebarLogo = !enable ? logo : logoAlt;
+    $("#brand img").attr("src", sidebarLogo);
 }
