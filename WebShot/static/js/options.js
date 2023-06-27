8d06997c65e603d97be13a4e5e2e05852ababb53
Corban-Lee <77944149+Corban-Lee@users.noreply.github.com>
2023-06-27 09:45:58 +0100

    resolution dropmenu controls

--- WebShot/static/js/options.js
+++ WebShot/static/js/options.js
@@ -94,3 +94,88 @@ $("#openNewResolutionCollapse").on("click", function() {
         $(this).text("Collapse");
     }
 });
+
+
+var sortCollapsed = true;
+$("#openSortResolutionCollapse").on("click", function() {
+
+    sortCollapsed = !sortCollapsed;
+
+    $(".resolution-collapse").each(function() {
+        const bootstrapCollapse = bootstrap.Collapse.getOrCreateInstance($(this));    
+        bootstrapCollapse.toggle();
+    });
+
+    var text;
+
+    if (!$("#resolutionDropdown ul").first().children().length) {
+        text = "Sort";
+        alert("Please add a new resolution first.");        
+    }
+
+    else if (sortCollapsed) {
+        text = "Sort"
+    }
+
+    else {
+        text = "Collapse";
+    }
+
+    $(this).text(text);
+});
+
+function addNewResolution(width, height) {
+    const displayName = `${width}x${height}`;
+    const id = `resolution_${displayName}`;
+    const showClass = sortCollapsed ? "": "show";
+
+    const resolutionContainer = $(`
+        <li class="dropdown-item p-0 d-flex">
+            <div class="resolution-item">
+                <button class="btn">
+                    <input id="${id}" type="checkbox" class="form-check-input">
+                </button>
+                <label for="${id}">
+                    ${width}
+                    <i class="bi bi-x"></i>
+                    ${height}
+                </label>
+                <button class="btn">
+                    <i class="bi bi-pc-display-horizontal"></i>
+                </button>
+            </div>
+            <div class="resolution-collapse collapse collapse-horizontal bg-body p-0 ${showClass}">
+                <div class="btn-group px-2 d-flex align-items-center h-100 my-auto">
+                    <button class="sort-resolution-up btn btn-sm btn-sidebar">
+                        <i class="bi bi-chevron-up"></i>
+                    </button>
+                    <button class="sort-resolution-down btn btn-sm btn-sidebar">
+                        <i class="bi bi-chevron-down"></i>
+                    </button>
+                </div>
+            </div>
+        </li>
+    `);
+    
+    $("#resolutionDropdown ul").first().append(resolutionContainer);
+
+    $(".sort-resolution-up").off("click").on("click", function() {
+        const resolutionContainer = $(this).closest(".dropdown-item");
+        resolutionContainer.prev().insertAfter(resolutionContainer);
+    });
+    
+    $(".sort-resolution-down").off("click").on("click", function() {
+        const resolutionContainer = $(this).closest(".dropdown-item");
+        resolutionContainer.next().insertBefore(resolutionContainer);
+    });
+}
+
+$("#createNewResolution").on("click", function() {
+    const width = $("#newResolutionWidth").val()
+    const height = $("#newResolutionHeight").val()
+    addNewResolution(width, height);
+});
+
+$(document).on("ready", function() {
+
+});
